6befd975dc4d030c68ce18fc9201efa0a2d42ec8
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-02-11 16:45:45 +0200

    Initial Commit

---
+++ Little Lemon Dinner Menu For Meta SwiftUI/Little_Lemon_Dinner_Menu_For_Meta_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_Dinner_Menu_For_Meta_SwiftUIApp.swift
+//  Little Lemon Dinner Menu For Meta SwiftUI
+//
+//  Created by Oleksandr Smakhtin on 11.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Dinner_Menu_For_Meta_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
